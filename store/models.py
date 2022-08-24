9751dfda7187449138f73c71d3dbcf0aa53b10d7
rachel <rachelkemuma99@gmail.com>
2022-08-24 17:32:42 +0300

    create class OrderItem in models

--- store/models.py
+++ store/models.py
@@ -21,8 +21,14 @@ class Product(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL,null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False)
-    transaction_id = models.CharField(max_length=100)
+    complete = models.BooleanField(default=False,null=True,blank=False)
+    transaction_id = models.CharField(max_length=100,null=True)
 
     def __str__(self):
         return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL,null=True)
+    quantity = models.IntegerField(default=0, null=True,blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
