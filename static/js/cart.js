4928e0598d68998f61c2f54ab2a7fbd24eee4f23
rachel <rachelkemuma99@gmail.com>
2022-08-25 18:10:34 +0300

    add a functionality that checks the status of the user

--- static/js/cart.js
+++ static/js/cart.js
@@ -5,5 +5,12 @@ for (i = 0; i< updateBtns.length; i++){
         let productId = this.dataset.product
         let action = this.dataset.action
         console.log('productId:', productId,'Action:',action)
+
+        console.log('USER:',user)
+        if(user === 'AnonymousUser'){
+            console.log('Not logged in')
+        } else{
+            console.log('User is logged in, sending data....')
+        }
     })
 }
