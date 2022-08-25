cea657efdd60ec22a6272d3eff75d8adad09db89
rachel <rachelkemuma99@gmail.com>
2022-08-25 18:32:46 +0300

    create function to update user order

--- static/js/cart.js
+++ static/js/cart.js
@@ -10,7 +10,25 @@ for (i = 0; i< updateBtns.length; i++){
         if(user === 'AnonymousUser'){
             console.log('Not logged in')
         } else{
-            console.log('User is logged in, sending data....')
+            updateUserOrder(productId, action)
+            // console.log('User is logged in, sending data....')
         }
     })
+}
+function updateUserOrder(productId, action){
+    console.log('User is authenticated, sending data...')
+        var url = '/update_item/'
+        fetch(url,{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json',
+            },
+            body:JSON.stringify({'productId':productId, 'action':action})
+        })
+        .then((response)=>{
+            return response.json()
+        })
+        .then((data)=>{
+            console.log('data:',data)
+        });
 }
