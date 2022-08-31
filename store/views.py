aac154288da3941da1258f0992fd0915a5e23c51
rachel <rachelkemuma99@gmail.com>
2022-08-31 15:52:10 +0300

    add a function that confirm total

--- store/views.py
+++ store/views.py
@@ -81,6 +81,10 @@ def processOrder(request):
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
     else:
         print('User is not logged in..')
     return JsonResponse('Payment complete',safe=False)
